007099a823cd7e539ea9f4c46f8c324c10374ca3
thatguy-lcgm <116634183+thatguy-lcgm@users.noreply.github.com>
2022-10-29 07:41:01 -0300

    Add files via upload

---
+++ ex060.py
@@ -0,0 +1,18 @@
+#Using while to calculate the factorial:
+n = int(input('Digit a value: '))
+cont = n
+factorial = 1
+while cont > 0:
+    print('{}'.format(cont), end='')
+    print(' x ' if cont > 1 else ' = ', end='')
+    factorial *= cont
+    cont -= 1
+print(factorial)
+#Using range to calculate the factorial:
+n = int(input('Digit a value: '))
+factorial = 1
+for c in range(n, 0, -1):
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    factorial *= c
+print(factorial)
